fae4f291daaab3daf165749dd24c850bdddd3355
Annin-Offei Michael <44599299+Anninoffei@users.noreply.github.com>
2024-09-05 13:39:27 -0400

    update

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
+const publishableKey = 'pk_test_dG9wcy1waWthLTYxLmNsZXJrLmFjY291bnRzLmRldiQ';
 export default clerkMiddleware(async (req, res, next) => {
   // Your Clerk logic here
 
